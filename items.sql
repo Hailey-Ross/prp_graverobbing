1d28f33a4475634a0abd008bb7f4ddc388fb5587
Hailey <26784904+Hailey-Ross@users.noreply.github.com>
2022-03-18 16:26:59 -0600

    Create items.sql

---
+++ items.sql
@@ -0,0 +1,7 @@
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('oldwatch', 'Tarnished Watch', 5, 1, 'item_standard', 0);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('rubyring', 'Old Ruby Ring', 8, 1, 'item_standard', 0);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('goldtooth', 'Gold Tooth', 12, 1, 'item_standard', 0);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('peacockfeather', 'Peacock Feather', 15, 1, 'item_standard', 0);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('piratecoin', 'Pirate Coin', 15, 1, 'item_standard', 0);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('oldbuckle', 'Rusty Belt Buckle', 15, 1, 'item_standard', 0);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('robbingkit', 'Grave Robbery Kit', 1, 1, 'item_standard', 1);

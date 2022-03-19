3d1b084dae09229083ed9cb91d70e09ea87fdb28
Hailey <26784904+Hailey-Ross@users.noreply.github.com>
2022-03-19 14:47:11 -0600

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,32 +1,67 @@
+VorpInv = exports.vorp_inventory:vorp_inventoryApi()
+
+local VorpCore = {}
+
 TriggerEvent("getCore",function(core)
     VorpCore = core
 end)
 
-VORP = exports.vorp_inventory:vorp_inventoryApi()
-
-VORP.RegisterUsableItem("robbingkit", function(data)
-        VORP.subItem(data.source, "robbingkit", 1)
+VorpInv.RegisterUsableItem("robbingkit", function(data)
+        VorpInv.subItem(data.source, "robbingkit", 1)
 		TriggerClientEvent('GraveRobbing:TriggerRobbery', data.source)
 end)
 
 RegisterNetEvent('wcrp:graverobbingreward')
 AddEventHandler('wcrp:graverobbingreward', function()
-    VORP.addItem(source, "peacockfeather", 1)
+    VorpInv.addItem(source, "peacockfeather", 1)
 
-    VORP.addItem(source, "piratecoin", 1)
+    VorpInv.addItem(source, "piratecoin", 1)
 end)
 
 RegisterNetEvent('wcrp:graverobbingreward2')
 AddEventHandler('wcrp:graverobbingreward', function()
-    VORP.addItem(source, "oldbuckle", 1)
+    VorpInv.addItem(source, "oldbuckle", 1)
 
-    VORP.addItem(source, "oldwatch", 1)
+    VorpInv.addItem(source, "oldwatch", 1)
 
-    VORP.addItem(source, "rubyring", 1)
+    VorpInv.addItem(source, "rubyring", 1)
 
-    VORP.addItem(source, "goldtooth", 3)
+    VorpInv.addItem(source, "goldtooth", 1)
 
-    VORP.addItem(source, "peacockfeather", 1)
+    VorpInv.addItem(source, "peacockfeather", 1)
 
-    VORP.addItem(source, "piratecoin", 2)
+    VorpInv.addItem(source, "piratecoin", 1)
+end)
+
+function keysx(table)
+    local keys = 0
+    for k,v in pairs(table) do
+       keys = keys + 1
+    end
+    return keys
+end
+
+RegisterServerEvent('wcrp:graverobbingreward3')
+AddEventHandler('wcrp:graverobbingreward3', function()
+	local _source = source
+	local Character = VorpCore.getUser(_source).getUsedCharacter
+	local chance =  math.random(1,10)
+	local reward = {}
+	for k,v in pairs(Config.Items) do 
+		if v.chance >= chance then
+			table.insert(reward,v)
+		end
+	end
+	local chance2 = math.random(1,keysx(reward))
+	local count = math.random(1,reward[chance2].amount)
+	TriggerEvent("vorpCore:canCarryItems", tonumber(_source), count, function(canCarry)
+		TriggerEvent("vorpCore:canCarryItem", tonumber(_source), reward[chance2].name,count, function(canCarry2)
+			if canCarry and canCarry2 then
+				VorpInv.addItem(_source, reward[chance2].name, count)
+				TriggerClientEvent("vorp:TipRight", _source, "You found "..count.." "..reward[chance2].label, 3000)
+			else
+				TriggerClientEvent("vorp:TipRight", _source, "You can't carry any more "..reward[chance2].label, 3000)
+			end
+		end)
+	end) 
 end)

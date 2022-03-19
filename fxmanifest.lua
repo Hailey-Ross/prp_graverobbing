fdc7ed27f9ceb8964b7fef0737f10b7b4e099c0a
Hailey <26784904+Hailey-Ross@users.noreply.github.com>
2022-03-19 14:47:25 -0600

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,4 +3,5 @@ games {"rdr3"}
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 client_script 'client.lua'
+shared_script 'config.lua'
 server_script 'server.lua'

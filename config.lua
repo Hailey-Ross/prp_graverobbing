c91f7933338b6461d99239193ae7109221dad73d
Hailey <26784904+Hailey-Ross@users.noreply.github.com>
2022-03-19 14:46:53 -0600

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,10 @@
+Config = {}
+
+Config.Items = {
+    {name = "oldbuckle", label = "Rusty Belt Buckle", chance = 10, amount = 4},
+    {name = "oldwatch", label = "Tarnished Watch", chance = 10, amount = 2},
+    {name = "rubyring", label = "Old Ruby Ring", chance = 10, amount = 2},
+    {name = "goldtooth", label = "Gold Tooth", chance = 10, amount = 3},
+    {name = "peacockfeather", label = "Peacock Feather", chance = 10, amount = 4},
+    {name = "piratecoin", label = "Pirate Coin", chance = 10, amount = 7},
+}

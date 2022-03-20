98429e9a2725e707e6a85aa6534b75fdbd7e214b
Hailey <26784904+Hailey-Ross@users.noreply.github.com>
2022-03-19 19:27:38 -0600

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,6 +2,8 @@ fx_version "adamant"
 games {"rdr3"}
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
+dependency 'weathersync'
+
 client_script 'client.lua'
 shared_script 'config.lua'
 server_script 'server.lua'

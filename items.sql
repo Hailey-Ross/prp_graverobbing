d880874f9c96936f30e5cdc58f4047af9aa0e31b
Hailey <26784904+Hailey-Ross@users.noreply.github.com>
2022-03-19 20:10:19 -0600

    Update items.sql

--- items.sql
+++ items.sql
@@ -5,3 +5,9 @@ INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VA
 INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('piratecoin', 'Pirate Coin', 15, 1, 'item_standard', 0);
 INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('oldbuckle', 'Rusty Belt Buckle', 15, 1, 'item_standard', 0);
 INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('robbingkit', 'Grave Robbery Kit', 1, 1, 'item_standard', 1);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('bone', 'An old Bone', 5, 1, 'item_standard', 0);
+INSERT IGNORE INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('rollingpaper', 'Rolling Paper', 20, 1, 'item_standard', 1);
+INSERT IGNORE INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('rock', 'Rock', 20, 1, 'item_standard', 1);
+INSERT IGNORE INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('pipe', 'Pipe', 3, 1, 'item_standard', 1);
+INSERT IGNORE INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('beer', 'Beer', 12, 1, 'item_standard', 1);
+INSERT IGNORE INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('book', 'Book', 5, 1, 'item_standard', 1);

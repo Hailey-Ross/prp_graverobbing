8570e386468413397b3a23ed37b3aa919a640252
Hailey <26784904+Hailey-Ross@users.noreply.github.com>
2022-03-19 20:07:15 -0600

    Update server.lua

--- server.lua
+++ server.lua
@@ -29,13 +29,38 @@ function keysx(table)
     return keys
 end
 
-RegisterServerEvent('wcrp:graverobbingreward3')
-AddEventHandler('wcrp:graverobbingreward3', function()
+RegisterServerEvent('wcrp:graverobbingreward')
+AddEventHandler('wcrp:graverobbingreward', function()
 	local _source = source
 	local Character = VorpCore.getUser(_source).getUsedCharacter
 	local chance =  math.random(1,10)
 	local reward = {}
 	for k,v in pairs(Config.Items) do 
+		if v.chance >= chance then
+			table.insert(reward,v)
+		end
+	end
+	local chance2 = math.random(1,keysx(reward))
+	local count = math.random(1,reward[chance2].amount)
+	TriggerEvent("vorpCore:canCarryItems", tonumber(_source), count, function(canCarry)
+		TriggerEvent("vorpCore:canCarryItem", tonumber(_source), reward[chance2].name,count, function(canCarry2)
+			if canCarry and canCarry2 then
+				VorpInv.addItem(_source, reward[chance2].name, count)
+				TriggerClientEvent("vorp:TipRight", _source, "You found "..count.." "..reward[chance2].label, 3000)
+			else
+				TriggerClientEvent("vorp:TipRight", _source, "You can't carry any more "..reward[chance2].label, 3000)
+			end
+		end)
+	end) 
+end)
+
+RegisterServerEvent('wcrp:graverobbingreward2')
+AddEventHandler('wcrp:graverobbingreward2', function()
+	local _source = source
+	local Character = VorpCore.getUser(_source).getUsedCharacter
+	local chance =  math.random(1,10)
+	local reward = {}
+	for k,v in pairs(Config.Items2) do 
 		if v.chance >= chance then
 			table.insert(reward,v)
 		end

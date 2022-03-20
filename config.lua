5b5f79ac868a38be8108b9f0bc9a6088171da256
Hailey <26784904+Hailey-Ross@users.noreply.github.com>
2022-03-19 20:07:02 -0600

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,6 +2,7 @@ Config = {}
 
 Config.Daytime = "You get the feeling this might be a bad idea right now. ."
 
+
 Config.Items = {
     {name = "oldbuckle", label = "Rusty Belt Buckle", chance = 10, amount = 4},
     {name = "oldwatch", label = "Tarnished Watch", chance = 10, amount = 2},
@@ -9,4 +10,13 @@ Config.Items = {
     {name = "goldtooth", label = "Gold Tooth", chance = 10, amount = 3},
     {name = "peacockfeather", label = "Peacock Feather", chance = 10, amount = 4},
     {name = "piratecoin", label = "Pirate Coin", chance = 10, amount = 7},
+}
+
+Config.Items2 = {
+    {name = "rollingpaper", label = "Rolling Paper", chance = 10, amount = 3},
+    {name = "rock", label = "Rock", chance = 10, amount = 2},
+    {name = "pipe", label = "Pipe", chance = 10, amount = 1},
+    {name = "beer", label = "Old Beer", chance = 10, amount = 1},
+    {name = "bone", label = "An old Bone", chance = 10, amount = 2},
+    {name = "book", label = "A dusty Book", chance = 10, amount = 1},
 }

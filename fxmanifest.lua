803b1227a7f1103e3e84905db0a3bf29dd3b4efa
Hailey <26784904+Hailey-Ross@users.noreply.github.com>
2022-03-31 12:27:15 -0600

    Add dependency syn_alert

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,6 +3,7 @@ games {"rdr3"}
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 dependency 'weathersync'
+dependency 'syn_alert'
 
 client_script 'client.lua'
 shared_script 'config.lua'

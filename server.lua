7691c4cd8347b049f2056fcfa1f04f5275d9f5af
Hailey <26784904+Hailey-Ross@users.noreply.github.com>
2022-12-23 16:42:59 -0700

    Better Math(s)?
    Minor fix to Seed Generation

--- server.lua
+++ server.lua
@@ -5,7 +5,7 @@ local MasterCeiling = Ceiling + Ceiling
 local testsuccess, result = pcall(os.time)
 
 if testsuccess then
- 	math.randomseed(os.time() + math.random(Floor,Ceiling))
+ 	math.randomseed(os.time() + math.random(Floor,Ceiling) + math.random(Floor,Ceiling) * math.random(1,3) - math.random(Floor,MasterCeiling))
 	if Config.Debug == true then print("os.time PASSED Test.") end
 else
  	math.randomseed(math.random(Floor,Ceiling) + math.random(Floor,Ceiling) * math.random(1,3) - math.random(Floor,MasterCeiling))

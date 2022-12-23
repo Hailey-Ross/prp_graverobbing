5c4545246b4ed9bb27f371fb13d56e38b4938d9a
Hailey <26784904+Hailey-Ross@users.noreply.github.com>
2022-12-23 11:13:03 -0700

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,4 +1,5 @@
 VorpInv = exports.vorp_inventory:vorp_inventoryApi()
+math.randomseed(os.time() + math.random(Config.SeedFloor,Config.SeedCeiling))
 
 local VorpCore = {}
 
@@ -23,6 +24,16 @@ VorpInv.RegisterUsableItem("robbingkit", function(data)
 	local maxchancex = maxchance + maxchance
 	local breakroll = math.random(minchance,maxchance)
 	local breakroll2 = math.random(minchance,maxchance)
+	local targetJob = Config.RequiredOnlineJob
+	local players = VorpCore.GetPlayers()
+	local jobcount = 0
+
+	for i, player in ipairs(players) do
+  		local playerJob = VorpCore.GetPlayerJob(player)
+  		if playerJob == targetJob then jobcount = jobcount + 1 end
+	end
+		
+	if Config.RequireLawmen == true and jobcount <= 1 then --Main Loop for Requiring Lawmen
 	if WorldTime.hour == 22 or WorldTime.hour == 23 or WorldTime.hour == 0 or WorldTime.hour == 1 or WorldTime.hour == 2 or WorldTime.hour == 3 or WorldTime.hour == 4 or WorldTime.hour == 5 then
 		if Config.DisableBreaking == false then
 			local breakchance = breakroll + breakroll2
@@ -44,6 +55,33 @@ VorpInv.RegisterUsableItem("robbingkit", function(data)
 		TriggerClientEvent("vorp:TipBottom", data.source, Config.Daytime, 5000)
 		if Config.Debug == true then print("Attempted daytime Graverobbing") end
 	end
+	elseif Config.RequireLawmen == False then -- Main Loop not requiring Lawmen
+			if WorldTime.hour == 22 or WorldTime.hour == 23 or WorldTime.hour == 0 or WorldTime.hour == 1 or WorldTime.hour == 2 or WorldTime.hour == 3 or WorldTime.hour == 4 or WorldTime.hour == 5 then
+		if Config.DisableBreaking == false then
+			local breakchance = breakroll + breakroll2
+			if Config.Debug == true then print(breakroll.." + "..breakroll2) end
+			if breakchance >= 310 then
+				VorpInv.subItem(data.source, "robbingkit", 1)
+				TriggerClientEvent("vorp:TipRight", data.source, Config.LostKit, 3000)
+				TriggerClientEvent('GraveRobbing:TriggerRobbery', data.source)
+				if Config.Debug == true then print("Kit Broken: Rolled "..breakchance.."/"..maxchancex) end
+			else
+				TriggerClientEvent('GraveRobbing:TriggerRobbery', data.source)
+				if Config.Debug == true then print("Kit did not break, Rolled: "..breakchance.."/"..maxchancex) end
+			end
+		else
+			TriggerClientEvent('GraveRobbing:TriggerRobbery', data.source)
+			if Config.Debug == true then print("Kit cannot be broken. Config Disable Breaking is TRUE") end
+		end
+	else
+		TriggerClientEvent("vorp:TipBottom", data.source, Config.Daytime, 5000)
+		if Config.Debug == true then print("Attempted daytime Graverobbing") end
+		end
+	end
+	else
+		TriggerClientEvent("vorp:TipBottom", data.source, Config.NoLawmenOnline, 5000)
+		if Config.Debug == true then print("Attempted graverobbing without Lawmen awake") end
+		end
 end)
 
 function keysx(table)
